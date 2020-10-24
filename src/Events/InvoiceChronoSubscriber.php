3efa818bc337a1579d969a696a3522a47cd227b9
Mikaia Ralaivita <mikaiajonathan@gmail.com>
2020-10-24 17:17:37 +0200

    Handle specific case on Kernel lifecycle and extend Doctrine

---
+++ src/Events/InvoiceChronoSubscriber.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Events;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\Invoice;
+use App\Repository\InvoiceRepository;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Security;
+
+class InvoiceChronoSubscriber implements EventSubscriberInterface
+{
+    /** @var Security */
+    private $security;
+
+    /** @var InvoiceRepository */
+    private $repository;
+
+    public function __construct(Security $security, InvoiceRepository $repository)
+    {
+        $this->security = $security;
+        $this->repository = $repository;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::VIEW => ["setChronoForInvoice", EventPriorities::PRE_VALIDATE]
+        ];
+    }
+
+    public function setChronoForInvoice(ViewEvent $event)
+    {
+        $invoice = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        if ($invoice instanceof Invoice && $method === "POST") {
+            $nextChrono = $this->repository->getNextChrono($this->security->getUser());
+            $invoice->setChrono($nextChrono);
+        }
+    }
+}
